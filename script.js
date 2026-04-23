77d573258530862abeba3d2fea3b8daa9746a08a
Bakeixd <acafrot79@naver.com>
2026-04-23 22:29:21 +0900

    init: full upload

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+
+  const elements = document.querySelectorAll('.section');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  elements.forEach(el => observer.observe(el));
+
+});
+</script>
